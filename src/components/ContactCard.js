5831afdea41487d45b3774b267d186458c0b25de
SAMUDHRA <samuthrabs@gmail.com>
2024-08-02 17:53:57 +0530

    contact list is created and stored in local storage

---
+++ src/components/ContactCard.js
@@ -0,0 +1,21 @@
+import React from "react";
+import user from "../images/user.png";
+
+const ContactCard = (props) => {
+    const { id, name, email } = props.contact;
+    return (
+        <div className="item">
+            <div className="right floated content">
+                <i className="trash alternate outline icon" style={{ color: "red", cursor: "pointer", marginTop: "7px"}}></i>
+            </div>
+            <img className="ui avatar image" src={user} alt="user" />
+            <div className="content">
+                <div className="header">{name}</div>
+                <div>{email}</div>
+            </div>
+        </div>
+    );
+}
+
+
+export default ContactCard;

9c930fb8fed6baf2cb1c7bc4cb09ef3801c07834
SAMUDHRA <samuthrabs@gmail.com>
2024-08-03 03:14:59 +0530

    Routing is done

--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -1,24 +1,67 @@
 import React from "react";
 import ContactCard from "./ContactCard";
 
-
 const ContactList = (props) => {
-    console.log(props);
+    console.log(props.contacts); 
 
-        const deleteContactHandler = (id) => {
-            props.getContactId(id);
-        };
+    const deleteContactHandler = (id) => {
+        props.getContactId(id);
+    };
+
     const renderContactList = props.contacts.map((contact) => {
         return (
-           <ContactCard contact = {contact} clickHandler = {deleteContactHandler} key = {contact.id}></ContactCard>
+            <ContactCard 
+                contact={contact} 
+                clickHandler={deleteContactHandler} 
+                key={contact.id}
+            />
         );
-    })
-    return(
-        <div className="ui celled list">
-            <h2>Contact List</h2>
-            {renderContactList}</div>
-    );
-}
+    });
 
+    return (
+        <div className="main">
+            <h2 style={{ marginTop: "50px"}}>Contact List
+            {/* <button className="ui button blue right">Add Contact</button> */}
+            </h2>
+            <div className="ui celled list">{renderContactList}</div>
+        </div>
+    );
+};
 
 export default ContactList;
+
+// import React from "react";
+// import ContactCard from "./ContactCard";
+
+
+// const ContactList = (props) => {
+//     console.log(props);
+
+//     const deleteContactHandler = (id) => {
+//         props.getContactId(id);
+//     };
+//     const contacts = [
+//         {
+//             id: "1",
+//             name: "samudhra",
+//             email: "samuthrabs@gmail.com"
+//         },
+//     ];
+//     const renderContactList = contacts.map((contact) => {
+//         return (
+//             <ContactCard 
+//                 contact = {contact} 
+//                 clickHandler = {deleteContactHandler} 
+//                 key = {contact.id}></ContactCard>
+//         );
+//     })
+//     return(
+//         <div className="main">
+//             <h2>Contact List</h2>
+//             <div className="ui celled list">{renderContactList}</div>
+//         </div>
+//     );
+// };
+
+
+// export default ContactList;
